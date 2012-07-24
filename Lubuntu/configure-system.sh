d8e7d924a1394b8060561dabefb41397e7a3102f
Christian Halstrick <christian.halstrick@sap.com>
2012-07-24 11:36:54 +0200

    adding installation of eclipse exec envs

--- Lubuntu/configure-system.sh
+++ Lubuntu/configure-system.sh
@@ -64,8 +64,8 @@ EOF
 
 # install egit/jgit in juno
 eclipse-juno -application org.eclipse.equinox.p2.director \
-	-r http://download.eclipse.org/egit/updates,http://download.eclipse.org/releases/juno \
-	-i org.eclipse.egit.feature.group,org.eclipse.egit.import.feature.group,org.eclipse.egit.mylyn.feature.group,org.eclipse.egit.psf.feature.group,org.eclipse.jgit.feature.group,org.eclipse.jgit.pgm.feature.group,org.eclipse.mylyn.gerrit.feature.feature.group,org.eclipse.mylyn.git.feature.group,org.eclipse.mylyn.github.feature.feature.group,org.eclipse.cdt.autotools.feature.group,org.eclipse.cdt.feature.group,org.eclipse.m2e.feature.feature.group
+	-r http://download.eclipse.org/egit/updates,http://download.eclipse.org/releases/juno,http://download.eclipse.org/eclipse/updates/4.2-I-builds  \
+	-i org.eclipse.egit.feature.group,org.eclipse.egit.import.feature.group,org.eclipse.egit.mylyn.feature.group,org.eclipse.egit.psf.feature.group,org.eclipse.jgit.feature.group,org.eclipse.jgit.pgm.feature.group,org.eclipse.mylyn.gerrit.feature.feature.group,org.eclipse.mylyn.git.feature.group,org.eclipse.mylyn.github.feature.feature.group,org.eclipse.cdt.autotools.feature.group,org.eclipse.cdt.feature.group,org.eclipse.m2e.feature.feature.group,org.eclipse.pde.api.tools.ee.j2se15.group,org.eclipse.pde.api.tools.ee.javase16.group,org.eclipse.pde.api.tools.ee.javase17.group   
 
 # install egit/jgit in indigo (not all components
 eclipse -application org.eclipse.equinox.p2.director \
