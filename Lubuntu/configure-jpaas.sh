a5e4bf807d134f8c6f1ebe174aa4673e27febfcd
chalstrick <christian.halstrick@gmail.com>
2012-09-16 10:27:34 +0300

    Update Lubuntu/configure-jpaas.sh

--- Lubuntu/configure-jpaas.sh
+++ Lubuntu/configure-jpaas.sh
@@ -3,24 +3,27 @@
 # Configure a Lubuntu12.04 system for jpaas development 
 #
 
+no_proxy='wdf.sap.corp, nexus, wiki, git.wdf.sap.corp'
 # setup mavens settings.xml
-if [ ! -f ~/.m2/settings.xml ] ;then
-	mkdir -p ~/.m2
-	wget -O ~/.m2/settings.xml http://nexus.wdf.sap.corp:8081/nexus/service/local/templates/settings/LeanDI/content
+if [ ! -f ~/.m2/settings.xml.jpaas ] ;then
+        mkdir -p ~/.m2
+        wget -O ~/.m2/settings.xml.jpaas http://nexus.wdf.sap.corp:8081/nexus/service/local/templates/settings/LeanDI/content
+        if [ ! -f ~/.m2/settings.xml ] ;then
+                cp ~/.m2/settings.xml.jpaas ~/.m2/settings.xml
+        else
+                echo "Couldn't set ~/.m2/settings.xml because there is already an settings.xml. Copied the jpaas settings.xml to ~/.m2/settings.xml.jpaas"
+        fi
 fi
 
 # clone & build metering
 mkdir -p ~/git
-git clone -q https://git.wdf.sap.corp:8080/NGJP/Services/metering
+https_proxy="" GIT_SSL_NO_VERIFY=true git clone https://git.wdf.sap.corp:8080/NGJP/Services/metering ~/git/metering
 cd ~/git/metering/com.sap.core.metering.parent && mvn install -DskipTests
 
 # setup a jpaas sdk
 if [ ! -d ~/jpaas/sdk1.9 ] ;then
-	mkdir -p ~/jpaas
-	echo "Please download a juno SDK from https://tools.prod.jpaas.sapbydesign.com/index.html to ~/jpaas"
+        mkdir -p ~/jpaas
+        echo "Please download a juno SDK from https://tools.prod.jpaas.sapbydesign.com/index.html to ~/jpaas"
 fi
 
-# install plugins to eclipse-juno
-eclipse-juno -application org.eclipse.equinox.p2.director \
-	-r http://download.eclipse.org/egit/updates,http://download.eclipse.org/releases/juno,http://download.eclipse.org/eclipse/updates/4.2  \
-	-i org.eclipse.egit.feature.group,org.eclipse.egit.import.feature.group,org.eclipse.egit.mylyn.feature.group,org.eclipse.egit.psf.feature.group,org.eclipse.jgit.feature.group,org.eclipse.jgit.pgm.feature.group,org.eclipse.mylyn.gerrit.feature.feature.group,org.eclipse.mylyn.git.feature.group,org.eclipse.mylyn.github.feature.feature.group,org.eclipse.cdt.autotools.feature.group,org.eclipse.cdt.feature.group,org.eclipse.m2e.feature.feature.group,org.eclipse.pde.api.tools.ee.j2se15.group,org.eclipse.pde.api.tools.ee.javase16.group,org.eclipse.pde.api.tools.ee.javase17.group
+exit
