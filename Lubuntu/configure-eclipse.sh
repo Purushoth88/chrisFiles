a49de59ee7d927ad972b5107cb0b5eadba12b867
Christian Halstrick <christian.halstrick@sap.com>
2012-09-19 19:22:31 +0200

    split eclipse installation and egit plugin

--- Lubuntu/configure-eclipse.sh
+++ Lubuntu/configure-eclipse.sh
@@ -27,16 +27,6 @@ Categories=Development;IDE;Java;
 EOF
 fi
 
-# install egit/jgit in juno
-eclipse-juno -application org.eclipse.equinox.p2.director \
-	-r http://download.eclipse.org/egit/updates,http://download.eclipse.org/releases/juno,http://download.eclipse.org/eclipse/updates/4.2  \
-	-i org.eclipse.egit.feature.group,org.eclipse.egit.import.feature.group,org.eclipse.egit.mylyn.feature.group,org.eclipse.egit.psf.feature.group,org.eclipse.jgit.feature.group,org.eclipse.jgit.pgm.feature.group,org.eclipse.mylyn.gerrit.feature.feature.group,org.eclipse.mylyn.git.feature.group,org.eclipse.mylyn.github.feature.feature.group,org.eclipse.cdt.autotools.feature.group,org.eclipse.cdt.feature.group,org.eclipse.m2e.feature.feature.group,org.eclipse.pde.api.tools.ee.j2se15.group,org.eclipse.pde.api.tools.ee.javase16.group,org.eclipse.pde.api.tools.ee.javase17.group
-
-# install egit/jgit in indigo (not all components)
-eclipse -application org.eclipse.equinox.p2.director \
-	-r http://download.eclipse.org/egit/updates,http://download.eclipse.org/releases/indigo \
-	-i org.eclipse.egit.feature.group,org.eclipse.egit.psf.feature.group,org.eclipse.jgit.feature.group,org.eclipse.jgit.pgm.feature.group
-
 # prepare API Baselines
 mkdir -p ~/egit-releases 
 rel=org.eclipse.egit.repository-2.0.0.201206130900-r
