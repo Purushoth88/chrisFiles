306b966c9fbd78687a741ee2d5e08565375f75fd
Christian Halstrick <christian.halstrick@sap.com>
2012-09-19 10:21:51 +0200

    adding more to netrc

--- Lubuntu/configure-chris.sh
+++ Lubuntu/configure-chris.sh
@@ -3,9 +3,19 @@
 # Do some configurations for chris
 #
 
+sudo apt-get install -q --yes terminator
+
 if [ -f ~/.netrc ] ;then
 	echo "Don't write ~/.netrc because it already exists"
 else
+machine meteringd032780bsap.prop.jpaas.sapbydesign.com
+login d032780
+p	read -s -p "Enter password for d032780 at meteringd032780bsap.prop.jpaas.sapbydesign.com: " epass
+	echo -e "machine meteringd032780bsap.prop.jpaas.sapbydesign.com\nlogin d032780\npassword $epass" >> ~/.netrc
+	echo
+	read -s -p "Enter password for P1323864547 at servermetermeter-meter.prod.jpaas.sapbydesign.com: " epass
+	echo -e "machine servermetermeter-meter.prod.jpaas.sapbydesign.com\nlogin P1323864547\npassword $epass" >> ~/.netrc
+	echo
 	read -s -p "Enter password for d032780 at git.wdf.sap.corp: " epass
 	echo -e "machine git.wdf.sap.corp\nlogin d032780\npassword $epass" >> ~/.netrc
 	echo
