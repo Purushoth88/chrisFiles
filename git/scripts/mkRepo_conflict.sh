00446a1657c18f1705710c13debb19a65a459874
Christian Halstrick <christian.halstrick@sap.com>
2012-10-02 11:18:33 +0200

    adding mkRepo_conflict.sh

---
+++ git/scripts/mkRepo_conflict.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -x
+
+git init
+echo -e 'line1\nline2\nline3' > a
+echo -e 'line1\nline2\nline3' > b 
+git add a b 
+git commit -m "adding a,b"
+git checkout -b side
+echo -e 'line1\nline2(side)\nline3' > a
+echo -e 'line1\nline2\nline3(side)' > b 
+git commit -a -m "modifying a,b on side"
+git checkout master
+echo -e 'line1\nline2(master)\nline3' > a
+echo -e 'line1(master)\nline2\nline3' > b 
+git commit -a -m "modifying a,b on master"
+git merge side
+echo -e 'line1\nline2(combined)\nline3' > a
+git reset b
