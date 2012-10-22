041302eecd481b3e5e02e9b9cbaea5f429f8c19f
Christian Halstrick <christian.halstrick@sap.com>
2012-10-22 18:18:30 +0200

    adding scripts to snapshot and to create submodule tests

--- git/scripts/mkRepo_Submodules.sh
+++ git/scripts/mkRepo_Submodules.sh
@@ -4,12 +4,28 @@
 base=$(pwd)
 for i in main feature1 feature2 subfeature1a subfeature1b ;do mkdir $i; cd $i; git init; echo $i>content.txt; git add content.txt; git commit -m "initial in $i" ; cd ..;done
 cd main
+../snapshot.sh "after initializing all repos"
 for i in feature1 feature2 ;do git submodule add $base/$i $i ;done
-git commit -m "adding feature1 and feature2 to main"
+../snapshot.sh "after adding submodules feature1, feature2 to main"
+git commit -m "adding feature1, feature2 to main"
+../snapshot.sh "after commiting the addition of submodules"
 cd ..
 cd feature1
 for i in subfeature1b subfeature1a ;do git submodule add $base/$i $i ;done
-git commit -m "adding subfeature1a and subfeature1b to feature"
-cd ..
-cd main
-git submodule update
+git commit -m "adding subfeature1a and subfeature1b to feature1"
+cd ../main
+git submodule update --recursive
+../snapshot.sh "after modifying all submodule feature1 and calling submodule update recursive"
+git submodule foreach --recursive git pull
+../snapshot.sh "after calling git submodule foreach recurssive git pull"
+cd feature1
+echo B > b
+git add b
+git commit -m "adding b"
+cd subfeature1b
+echo C > c
+git add c
+git commit -m "adding c"
+cd ../..
+git submodule update --recursive
+../snapshot.sh "after changing feature1,subfeature1b and calling git submodule update"
--- git/scripts/snapshot.sh
+++ git/scripts/snapshot.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
-
-[ -d .git ] || ( echo ".git folder not found" ; exit -1 )
-[ -d .git/snapshot ] || { mkdir -p .git/snapshot/_git ; git init .git/snapshot; touch -d "1974-01-01 00:00:00" .git/snapshot }
-
-find . -newer .git/snapshot -a ! -path './.git/snapshot*' | while read file ;do
-	echo "file:<<$file>>"
-	tgt=".git/snapshot/${file/#.\/.git\//_git/}"
-	if [ -d "$file" ] ;then
-		[ -f "$target" ] && rm -f "$target"
-		[ -e "$target" ] || { mkdir "$target" ; continue }
-		if [ -d "$target" ] ;then
-			find "$(target)" -depth -maxdepth 1 | while read child ;do
-				[ -e "$file/$child" ] || { git rm "$target/$child" ; continue }
-			done
-			continue
-		fi
+[ -d .git ] || { echo ".git folder not found"; exit -1; }
+[ -d .git/snapshot ] || { mkdir -p .git/snapshot/_git; git init .git/snapshot; touch -d "1974-01-01 00:00:00" .git/snapshot; }
+find . -newer .git/snapshot -a ! -path './.git/snapshot/*' | while read src; do
+	tgt=".git/snapshot/${src/#.\/.git\//_git/}"
+	[ "$tgt" = ".git/snapshot/./.git" ] && tgt=".git/snapshot/_git"
+	path=${tgt:14}
+#	echo "src=<<<$src>>>, tgt=<<<$tgt>>>, path=<<<$path>>>"
+	if [ -d "$src" ]; then
+		[ -f "$tgt" ] && rm -f "$tgt"
+		[ -e "$tgt" ] || { mkdir "$tgt"; continue; }
+		[ -d "$tgt" ] && find "$tgt" -depth -maxdepth 1 -mindepth 1 -printf "%f\n" | while read child; do
+#			echo "looking at child: <<<$child>>>"
+			[ "$src/$child" == "./_git" ] && continue
+			[ -e "$src/$child" ] || { cd .git/snapshot; git rm -r "$path/$child"; cd ../..; }
+		done
 	else
-		[ -e "$target" ] || ( cd .git/snapshot; git add "${target:14}" )
-		[ "$file" = ./.git/index ] && { git ls-files --cached -s > "$tgt" ; continue }
-		[[ "$file" =~ ./.git/objects/[0-9a-f]{2}/[0-9a-f]{38}$ ]] &&  { git cat-file -p ${file:15:2}${file:18:38} > "$tgt" ; continue }
-		cp "$file" "$tgt"
+		[ "$src" = ./.git/index ] && { git ls-files --cached -s > "$tgt"; continue; }
+		[[ "$src" =~ ./.git/objects/[0-9a-f]{2}/[0-9a-f]{38}$ ]] && { git cat-file -p ${src:15:2}${src:18:38} > "$tgt"; continue; }
+		cp "$src" "$tgt"
 	fi
 done
 touch .git/snapshot
 (
 	cd .git/snapshot
-	git commit -m "${1-snapshot taken at $(date)}"
+	git add .
+	git commit -m "${1-snapshot taken at $(date)}" --allow-empty
+	git log --numstat -1
 )
