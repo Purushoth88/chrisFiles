f5337bebc14627a0258d912fe04228aaa513834c
Christian Halstrick <christian.halstrick@sap.com>
2012-11-23 11:15:08 +0100

    refactored out configure-projects.sh

---
+++ scripts/Lubuntu/configure-projects.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Clone and build projects
+#
+
+# clone git e/jgit & gerrit
+mkdir -p ~/git
+[ -d ~/git/git ] || git clone -q https://github.com/git/git.git ~/git/git &
+[ -d ~/git/jgit ] || git clone -q https://git.eclipse.org/r/p/jgit/jgit ~/git/jgit &
+[ -d ~/git/egit ] || git clone -q https://git.eclipse.org/r/p/egit/egit ~/git/egit &
+[ -d ~/git/egit-github ] || git clone -q https://git.eclipse.org/r/p/egit/egit-github ~/git/egit-github &
+[ -d ~/git/egit-pde ] || git clone -q https://git.eclipse.org/r/p/egit/egit-pde ~/git/egit-pde &
+[ -d ~/git/gerrit ] || git clone -q https://gerrit.googlesource.com/gerrit ~/git/gerrit &
+
+wait
+
+# clone linux
+git clone -q http://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git ~/git/linux &
+
+# configure all gerrit repos to push to the review queue
+for i in jgit egit egit-pde egit-github ;do git config -f ~/git/$i/.git/config remote.origin.push HEAD:refs/for/master ;done 
+
+# build the projects
+(cd ~/git/gerrit && mvn package -DskipTests) &
+(cd ~/git/git && make configure && ./configure && make) &
+(cd ~/git/jgit && mvn install -DskipTests)
+(cd ~/git/jgit/org.eclipse.jgit.packaging && mvn install)
+(cd ~/git/egit && mvn -P skip-ui-tests install -DskipTests)
+(cd ~/git/egit-github && mvn install -DskipTests) &
+(cd ~/git/egit-pde && mvn install -DskipTests) &
+
+wait
+
--- scripts/Lubuntu/configure-system.sh
+++ scripts/Lubuntu/configure-system.sh
@@ -27,32 +27,6 @@ dpkg -s sun-java5-jdk || {
 	sudo apt-get update
 }
 
-# clone git e/jgit & gerrit
-mkdir -p ~/git
-[ -d ~/git/git ] || git clone -q https://github.com/git/git.git ~/git/git &
-[ -d ~/git/jgit ] || git clone -q https://git.eclipse.org/r/p/jgit/jgit ~/git/jgit &
-[ -d ~/git/egit ] || git clone -q https://git.eclipse.org/r/p/egit/egit ~/git/egit &
-[ -d ~/git/egit-github ] || git clone -q https://git.eclipse.org/r/p/egit/egit-github ~/git/egit-github &
-[ -d ~/git/egit-pde ] || git clone -q https://git.eclipse.org/r/p/egit/egit-pde ~/git/egit-pde &
-[ -d ~/git/gerrit ] || git clone -q https://gerrit.googlesource.com/gerrit ~/git/gerrit &
-
-wait
-
-# clone linux
-git clone -q http://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git ~/git/linux &
-
-# configure all gerrit repos to push to the review queue
-for i in jgit egit egit-pde egit-github ;do git config -f ~/git/$i/.git/config remote.origin.push HEAD:refs/for/master ;done 
-
-# build the projects
-(cd ~/git/gerrit && mvn package -DskipTests) &
-(cd ~/git/git && make configure && ./configure && make) &
-(cd ~/git/jgit && mvn install -DskipTests)
-(cd ~/git/jgit/org.eclipse.jgit.packaging && mvn install)
-(cd ~/git/egit && mvn -P skip-ui-tests install -DskipTests)
-(cd ~/git/egit-github && mvn install -DskipTests) &
-(cd ~/git/egit-pde && mvn install -DskipTests) &
-
 # add user to group which is allowed to read shared folders
 if id -G -n | grep vbox ;then 
 	sudo adduser $USER vboxsf
