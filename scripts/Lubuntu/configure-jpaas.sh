4707bf030c0f4121860ec68c429b7f2d5ee65827
Christian Halstrick <christian.halstrick@sap.com>
2012-11-23 14:03:40 +0100

    adding more comments on what to download for jpaas

--- scripts/Lubuntu/configure-jpaas.sh
+++ scripts/Lubuntu/configure-jpaas.sh
@@ -20,15 +20,19 @@ mkdir -p ~/git
 https_proxy="" GIT_SSL_NO_VERIFY=true git clone https://git.wdf.sap.corp:8080/NGJP/Services/metering ~/git/metering
 cd ~/git/metering/com.sap.core.metering.parent && mvn install -DskipTests
 
-# setup a jpaas sdk
-if [ ! -d ~/jpaas/sdk1.9 ] ;then
-        mkdir -p ~/jpaas
-        echo "Please download a juno SDK from https://tools.prod.jpaas.sapbydesign.com/index.html to ~/jpaas"
-fi
-
 # install sap tools in juno
 eclipse-juno -application org.eclipse.equinox.p2.director \
 	-r https://tools.prod.jpaas.sapbydesign.com/juno \
 	-i com.sap.ide.ui5.cloud.feature.feature.group,com.sap.core.tools.eclipse.server.feature.feature.group,com.sap.core.tools.eclipse.help.feature.feature.group
+
+
+# setup a jpaas sdk
+if [ ! -d ~/jpaas/sdk1.9 ] ;then
+	mkdir -p ~/jpaas
+	echo "Please download and extract a juno SDK from https://tools.prod.jpaas.sapbydesign.com/index.html to ~/jpaas"
+	echo "Please download and extract a maxdb from https://downloads.sdn.sap.com/maxdb/7_8/maxdb-all-linux-64bit-x86_64-7_8_02_28.tgz" }
+	echo "Please download and extract a RemoteJDBC driver and war from https://wiki.wdf.sap.corp/wiki/display/JavaPersistence/Remote+Access+to+Databases+in+the+NetWeaver+Cloud"
+	echo "Please download a hana studio installer from \\production.wdf.sap.corp\newdb\NewDB100\rel"
+fi
 
 exit
