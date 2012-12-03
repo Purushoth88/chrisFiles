87f7466d2ecbf9124d6197efc5ea876ce8d70d21
Christian Halstrick <christian.halstrick@sap.com>
2012-12-03 19:34:40 +0100

    fixed a typo in configure-chris.sh

--- scripts/Lubuntu/configure-chris.sh
+++ scripts/Lubuntu/configure-chris.sh
@@ -8,8 +8,8 @@ sudo apt-get install -q --yes terminator
 if [ -f ~/.netrc ] ;then
 	echo "Don't write ~/.netrc because it already exists"
 else
-	read -s -p "Enter password for d032780 at meteringd032780bsap.prop.jpaas.sapbydesign.com: " epass
-	echo -e "machine meteringd032780bsap.prop.jpaas.sapbydesign.com\nlogin d032780\npassword $epass" >> ~/.netrc
+	read -s -p "Enter password for d032780 at meteringd032780bsap.prod.jpaas.sapbydesign.com: " epass
+	echo -e "machine meteringd032780bsap.prod.jpaas.sapbydesign.com\nlogin d032780\npassword $epass" >> ~/.netrc
 	echo
 	read -s -p "Enter password for P1323864547 at servermetermeter-meter.prod.jpaas.sapbydesign.com: " epass
 	echo -e "machine servermetermeter-meter.prod.jpaas.sapbydesign.com\nlogin P1323864547\npassword $epass" >> ~/.netrc
