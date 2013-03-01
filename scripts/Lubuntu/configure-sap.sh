d17089a4426795d387527c167caa9950330e6ecd
Christian Halstrick <christian.halstrick@sap.com>
2013-03-01 09:36:44 +0100

    fixed certificate import

--- scripts/Lubuntu/configure-sap.sh
+++ scripts/Lubuntu/configure-sap.sh
@@ -11,12 +11,10 @@ fi
 
 sudo -E apt-get -q=2 install libnss3-tools
 [ -f /media/sf_Shared/LubuntuConfig/SAPNetCA.crt ] && sudo cp /media/sf_Shared/LubuntuConfig/SAPNetCA.crt /usr/local/share/ca-certificates
-[ -f /media/sf_Shared/LubuntuConfig/SSO_CA.crt ] && sudo cp /media/sf_Shared/LubuntuConfig/SSO_CA.crt /usr/local/share/ca-certificates
 [ -r /usr/local/share/ca-certificates/SAPNetCA.crt ] && read -P "please put of copy of the SAPNetCA.crt file to /usr/local/share/ca-certificates and press return" 
-[ -r /usr/local/share/ca-certificates/SSO_CA.crt ] && read -P "please put of copy of the SSO_CA.crt file to /usr/local/share/ca-certificates and press return" 
 sudo dpkg-reconfigure ca-certificates
 sudo update-ca-certificates
-certutil -d sql:$HOME/.pki/nssdb -A -t TC -n "SSO_CA" -i /usr/local/share/ca-certificates/SSO_CA.crt
+[ -e $HOME/.pki/nssdb ] || ( mkdir -p $HOME/.pki/nssdb ; cd $HOME/.pki/nssdb ; certutil -N -d sql:. )
 certutil -d sql:$HOME/.pki/nssdb -A -t TC -n "SAPNetCA" -i /usr/local/share/ca-certificates/SAPNetCA.crt
 
 # add wdf.sap.corp as a default domain
