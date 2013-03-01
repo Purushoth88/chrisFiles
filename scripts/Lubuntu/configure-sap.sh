ceeec17dfe51711633ed554ff9065984e693a66e
Christian Halstrick <christian.halstrick@sap.com>
2013-03-01 09:48:52 +0100

    fixed certificate import

--- scripts/Lubuntu/configure-sap.sh
+++ scripts/Lubuntu/configure-sap.sh
@@ -12,6 +12,7 @@ fi
 sudo -E apt-get -q=2 install libnss3-tools
 [ -f /media/sf_Shared/LubuntuConfig/SAPNetCA.crt ] && sudo cp /media/sf_Shared/LubuntuConfig/SAPNetCA.crt /usr/local/share/ca-certificates
 [ -r /usr/local/share/ca-certificates/SAPNetCA.crt ] && read -P "please put of copy of the SAPNetCA.crt file to /usr/local/share/ca-certificates and press return" 
+sudo chmod 444 /usr/local/share/ca-certificates/SAPNetCA.crt
 sudo dpkg-reconfigure ca-certificates
 sudo update-ca-certificates
 [ -e $HOME/.pki/nssdb ] || ( mkdir -p $HOME/.pki/nssdb ; cd $HOME/.pki/nssdb ; certutil -N -d sql:. )
