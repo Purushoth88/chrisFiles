0e6a89cc479d427cb3355e6cad17bde83ab102ff
Christian Halstrick <christian.halstrick@sap.com>
2013-04-19 13:02:37 +0200

    fixed proxy script. added -on -off

--- scripts/Lubuntu/configure-sap.sh
+++ scripts/Lubuntu/configure-sap.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 #
+# Configure a Lubuntu system to work inside the SAP intranet.
+
+# create a script which sets the proxy-related stuff
+if [ ! -f ~/bin/setProxy.sh ] ;then
+	mkdir -p ~/bin
+	cat <<EOF >~/bin/setProxy.sh
+#!/bin/bash
+#
 # Configure a Lubuntu12.04 to use SAP proxy
 #
 
@@ -86,3 +94,24 @@ else
 	cmp -s /usr/share/applications/chromium-browser.desktop ~/.chromium-browser-proxy.desktop && sudo cp ~/.chromium-browser-noproxy.desktop /usr/share/applications/chromium-browser.desktop
 	echo "turned proxy usage OFF"
 fi
+EOF
+	chmod +x ~/bin/setProxy.sh
+fi
+
+# set proxy related stuff
+[ -x ~/bin/setProxy.sh ] && . ~/bin/setProxy.sh
+
+sudo -E apt-get -q=2 install libnss3-tools
+[ -f /media/sf_Shared/LubuntuConfig/SAPNetCA.crt ] && sudo cp /media/sf_Shared/LubuntuConfig/SAPNetCA.crt /usr/local/share/ca-certificates
+[ -r /usr/local/share/ca-certificates/SAPNetCA.crt ] && read -P "please put of copy of the SAPNetCA.crt file to /usr/local/share/ca-certificates and press return" 
+sudo chmod 444 /usr/local/share/ca-certificates/SAPNetCA.crt
+sudo dpkg-reconfigure ca-certificates
+sudo update-ca-certificates
+[ -e $HOME/.pki/nssdb ] || ( mkdir -p $HOME/.pki/nssdb ; cd $HOME/.pki/nssdb ; certutil -N -d sql:. )
+certutil -d sql:$HOME/.pki/nssdb -A -t TC -n "SAPNetCA" -i /usr/local/share/ca-certificates/SAPNetCA.crt
+
+# add wdf.sap.corp as a default domain
+if ! grep "^search" /etc/resolvconf/resolv.conf.d/base ;then
+	sudo sh -c 'echo "search wdf.sap.corp" >> /etc/resolvconf/resolv.conf.d/base'
+fi
+
