76cb17d3c2388d36e1e2438d94d90e500da55b26
Christian Halstrick <christian.halstrick@sap.com>
2013-05-02 15:38:20 +0200

    added dist-upgrade and clean to configure-system.sh

--- scripts/Lubuntu/configure-system.sh
+++ scripts/Lubuntu/configure-system.sh
@@ -5,6 +5,9 @@
 # update the package index database
 sudo -E apt-get -q=2 update
 
+# update installed packages, remove orphans
+sudo -E apt-get -q=2 dist-upgrade
+
 # install applications
 sudo -E apt-get -q=2 install git gitk vim vim-gui-common maven openjdk-7-{jdk,doc,source} visualvm curl firefox
 
@@ -17,3 +20,6 @@ fi
 
 # fix errors in jvisualvm
 [ -f /usr/bin/jvisualvm ] && sudo sed -r -i 's&/usr/share/netbeans/platform12/&/usr/share/netbeans/platform13/&' /usr/bin/jvisualvm
+
+# clean apt-get cache
+sudo -E apt-get -q=2 clean
