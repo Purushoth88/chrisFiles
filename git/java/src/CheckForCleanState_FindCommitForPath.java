1e47af942001f42e60a310b8af401f66da6f8e4e
Christian Halstrick <christian.halstrick@sap.com>
2013-07-26 15:49:23 +0200

    add CheckForCleanState_FindCommitForPath.java

--- git/java/src/CheckForCleanState_FindCommitForPath.java
+++ git/java/src/CheckForCleanState_FindCommitForPath.java
@@ -4,17 +4,24 @@ import java.io.IOException;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.JGitInternalException;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.RepositoryBuilder;
+import org.eclipse.jgit.lib.RepositoryCache;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.util.FS;
 
 public class CheckForCleanState_FindCommitForPath {
 	public static void main(String args[]) throws IOException, GitAPIException,
 			JGitInternalException {
 		
-		Git git = Git.open(new File(args[0]));
+		FS fs = FS.DETECTED;
+				RepositoryCache.FileKey key;
+		key = RepositoryCache.FileKey.lenient(new File(args[0]), fs);
+		Repository repo = new RepositoryBuilder().setFS(fs).setGitDir(key.getFile()).setMustExist(true).build();
 		if (!git.status().call().isClean())
 			throw new IllegalStateException("Repo state is not clean.");
 		// find last commit
 		for (RevCommit commit : git.log().addPath(args[1]).setMaxCount(1).call())
-			System.out.println("A commit touching path "+args[1]+" in repo "+git+": "+commit.getId());
+			System.out.println("A commit touching path "+args[1]+" in repo "+git.getRepository()+": "+commit.getId());
 	}
 }
