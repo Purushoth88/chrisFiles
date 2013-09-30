e2ae616141a82e83191e88af83b6611dd17662a0
Christian Halstrick <christian.halstrick@sap.com>
2013-09-30 18:05:56 +0200

    fix deleteOldProposal.sh

--- git/scripts/deleteOldProposals.sh
+++ git/scripts/deleteOldProposals.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
-for i in `git tag -l change/*/* | sort -t/ -n -k 2 -k 3 -r`
-do
-	if [ ${i%/*} == ${lastProp-""} ]; then
-		git tag -d $i
+#
+# Delete unneeded refs
+
+oldChangeNr="0"
+oldPatchSet="0"
+git show-ref | cut  -d ' ' -f 2 | grep '^refs/[^/]\+/change/[0-9]\+/[0-9]\+$' | sort -n -t/ -k4,5 -r | while read ref ;do
+	change=${ref##refs/*/change/}
+ 	changeNr=${change%%/*}
+	patchSet=${change##*/}
+	echo "found patcheset $patchSet of change $changeNr. ref=$ref."
+	if [[ "$changeNr" == "$oldChangeNr" ]] && [[ "$patchSet" -lt "$oldPatchSet" ]] ;then
+		echo "delete patcheset $patchSet of change $changeNr."
+		if [[ $ref == refs/heads/* ]] ;then
+			git branch -D ${ref##refs/heads/}
+		elif [[ $ref == refs/tags/* ]] ;then
+			git tag -d ${ref##refs/tags/}
+		fi
 	fi
-	lastProp=${i%/*}
+	oldPatchSet=$patchSet
+	oldChangeNr=$changeNr
 done
+
