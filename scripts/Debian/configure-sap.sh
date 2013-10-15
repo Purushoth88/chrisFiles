1e595418122aa01e77c3403bfbd96ccaea4b1da1
Christian Halstrick <christian.halstrick@gmail.com>
2013-10-15 17:42:29 +0200

    ask the user for the location of sap ca

--- scripts/Debian/configure-sap.sh
+++ scripts/Debian/configure-sap.sh
@@ -3,13 +3,18 @@
 # Configure a Lubuntu system to work inside the SAP intranet.
 
 sudo -E apt-get -q=2 install libnss3-tools
-[ -f /media/sf_Shared/LubuntuConfig/SAPNetCA.crt ] && sudo cp /media/sf_Shared/LubuntuConfig/SAPNetCA.crt /usr/local/share/ca-certificates
-[ -r /usr/local/share/ca-certificates/SAPNetCA.crt ] && read -P "please put of copy of the SAPNetCA.crt file to /usr/local/share/ca-certificates and press return" 
-sudo chmod 444 /usr/local/share/ca-certificates/SAPNetCA.crt
-sudo dpkg-reconfigure ca-certificates
-sudo update-ca-certificates
-[ -e $HOME/.pki/nssdb ] || ( mkdir -p $HOME/.pki/nssdb ; cd $HOME/.pki/nssdb ; certutil -N -d sql:. )
-certutil -d sql:$HOME/.pki/nssdb -A -t TC -n "SAPNetCA" -i /usr/local/share/ca-certificates/SAPNetCA.crt
+read -p "Folder containing SAPNetCA.crt: " crtFolder
+
+[ -f $crtFolder/SAPNetCA.crt ] && sudo cp $crtFolder/SAPNetCA.crt /usr/local/share/ca-certificates
+if [ -r /usr/local/share/ca-certificates/SAPNetCA.crt ] ;then
+	sudo chmod 444 /usr/local/share/ca-certificates/SAPNetCA.crt
+	sudo dpkg-reconfigure ca-certificates
+	sudo update-ca-certificates
+	[ -e $HOME/.pki/nssdb ] || ( mkdir -p $HOME/.pki/nssdb ; cd $HOME/.pki/nssdb ; certutil -N -d sql:. )
+	certutil -d sql:$HOME/.pki/nssdb -A -t TC -n "SAPNetCA" -i /usr/local/share/ca-certificates/SAPNetCA.crt
+else
+	echo "No valid SAPNetCA.crt found. Will not update the certificates databases"
+fi
 
 # add wdf.sap.corp as a default domain
 if ! grep "^search" /etc/resolvconf/resolv.conf.d/base ;then
