49988c488175b9756cf411aac26250ed4642d7aa
Christian Halstrick <christian.halstrick@gmail.com>
2013-10-15 16:13:00 +0200

    added sap remotes to jgit,gerrit

--- scripts/Lubuntu/configure-chris.sh
+++ scripts/Lubuntu/configure-chris.sh
@@ -62,7 +62,8 @@ else
 	cp ~/git/chrisFiles/vim/_vimrc ~/.vimrc
 fi
 
-(cd ~/git/jgit && git config remote.origin.pushurl https://chalstrick@git.eclipse.org/r/jgit/jgit.git && git remote add github https://chalstrick@github.com/chalstrick/jgit.git && git fetch github)
+(cd ~/git/jgit && git config remote.origin.pushurl https://chalstrick@git.eclipse.org/r/jgit/jgit.git && git remote add sap https://d032780@git.wdf.sap.corp:8080/NGP/LDI/jgit && git remote add github https://chalstrick@github.com/chalstrick/jgit.git && git fetch github sap && git config remote.sap.push HEAD:refs/for/master)
 (cd ~/git/egit && git config remote.origin.pushurl https://chalstrick@git.eclipse.org/r/egit/egit.git && git remote add github https://chalstrick@github.com/chalstrick/egit.git && git fetch github)
+(cd ~/git/gerrit && git remote add sap https://d032780@git.wdf.sap.corp:8080/NGP/LDI/gerrit-internal && git fetch sap && git config remote.sap.push HEAD:refs/for/master)
 git config -f ~/git/egit-pde/.git/config remote.origin.pushurl https://chalstrick@git.eclipse.org/r/egit/egit-pde.git
 git config -f ~/git/egit-github/.git/config remote.origin.pushurl https://chalstrick@git.eclipse.org/r/egit/egit-github.git
