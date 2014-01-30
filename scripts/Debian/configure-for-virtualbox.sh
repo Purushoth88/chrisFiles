89bf241bc7db79e31e6f75263dea90e32dcbd11a
Christian Halstrick <christian.halstrick@gmail.com>
2014-01-30 10:36:21 +0100

    update virtualbox guest additions install

--- scripts/Debian/configure-for-virtualbox.sh
+++ scripts/Debian/configure-for-virtualbox.sh
@@ -3,10 +3,11 @@
 # Configure a Debian based Linux to work properly as a VirtualBox guest
 
 # install applications
+sudo -E apt-get -q=2 update
 sudo -E apt-get -q=2 install dkms
 
 # install guest additions (after dkms)
-[ -x /media/VBOXADDITIONS*/VBoxLinuxAdditions.run ] && sudo -E /media/VBOXADDITIONS*/VBoxLinuxAdditions.run
+[ -x /media/$USER/VBOXADDITIONS*/VBoxLinuxAdditions.run ] && sudo -E /media/$USER/VBOXADDITIONS*/VBoxLinuxAdditions.run
 
 # add user to group which is allowed to read shared folders
 id -G -n | grep vbox || sudo adduser $USER vboxsf
