b5bb51eddd36d7fe84fe017ea12efe736993f106
Christian Halstrick <christian.halstrick@sap.com>
2014-02-11 16:25:55 +0100

    renamed configure- to get-eclipse.sh

--- scripts/Debian/get-eclipse.sh
+++ scripts/Debian/get-eclipse.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
-# Configure a Eclipse on a ubuntu system to my needs
-#
+# Install & configure Eclipse on a ubuntu based system
 
 # install java
 sudo -E apt-get -q=2 install openjdk-7-{jdk,doc,source} 
@@ -19,6 +18,7 @@ if [ ! -x /usr/bin/eclipse ] ;then
 	sudo chown -R $USER:$USER /opt/eclipse
 	sudo ln -s /opt/eclipse/eclipse ~/bin/
 	if [ ! -f ~/.local/share/applications/eclipse.desktop ] ;then
+		mkdir -p ~/.local/share/applications
 		cat <<EOF >~/.local/share/applications/eclipse.desktop
 [Desktop Entry]
 Version=4.3.0
@@ -33,9 +33,11 @@ EOF
 	fi
 fi
 
-# prepare API Baselines
+# download an EGit release to be prepared for API Baselines
 mkdir -p ~/egit-releases
 rel=org.eclipse.egit.repository-3.2.0.201312181205-r
 if [ ! -d ~/egit-releases/$rel ] ;then
 	wget -q http://download.eclipse.org/egit/updates-3.2/$rel.zip && unzip -q $rel.zip -d ~/egit-releases/$rel && rm $rel.zip
 fi
+
+read -p "Please add source and javadoc attachments to the installed JRE's in eclipse"
