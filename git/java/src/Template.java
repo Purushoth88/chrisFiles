4493c9b20d1f5c46d90fc25616652da7fedb1c77
Christian Halstrick <christian.halstrick@sap.com>
2015-03-13 11:14:18 +0100

    Change templates

--- git/java/src/Template.java
+++ git/java/src/Template.java
@@ -1,25 +1,32 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Properties;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.ListBranchCommand.ListMode;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.JGitInternalException;
-import org.eclipse.jgit.api.errors.RefNotFoundException;
-import org.eclipse.jgit.lib.Ref;
-import org.eclipse.jgit.lib.TextProgressMonitor;
 import org.eclipse.jgit.revwalk.RevCommit;
 
 public class Template {
 	public static void main(String args[]) throws IOException, GitAPIException,
 			JGitInternalException {
-		Git git = Git.open(new File(args[0]));
-		RevCommit headCommit = git.log().setMaxCount(1).call().iterator()
-				.next();
-		System.out.println("repo:" + git.getRepository()
-				+ ", head-commit:" + headCommit);
-		System.out.println("git fetch: "+git.fetch().call().getMessages());
+		Path tmp = Files.createTempDirectory("JGitTest_"
+				+ Template.class.getName());
+		try (Git git = Git.cloneRepository().setDirectory(tmp.toFile())
+				.setURI(args[0]).setBare(false).call()) {
+			System.out.println("Repo " + args[0] + " clone to " + tmp);
+			for (RevCommit c : git.log().setMaxCount(10).call())
+				System.out.println("Commit:" + c.getShortMessage()+ " ("+c.getName()+")");
+		} finally {
+			rm(tmp.toFile());
+		}
+	}
+
+	static void rm(File f) {
+		if (f.isDirectory())
+			for (File c : f.listFiles())
+				rm(c);
+		f.delete();
 	}
 }
